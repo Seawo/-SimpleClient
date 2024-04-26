8525d3c72d12a948e22da3cc5184ccff9b14572e
Seawo <89066816+Seawo@users.noreply.github.com>
2024-04-26 18:59:39 +0900

    boss Pattern 정리

--- Assets/Scritps/Enemy/Enemy.cs
+++ Assets/Scritps/Enemy/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
@@ -23,10 +24,12 @@ public class Enemy : MonoBehaviour
 
     public float maxHealth = 100.0f;
     public float curHealth = 0.0f;
+    public float bossTermTime = 0.1f;
+    public float bossAniTime = 3.0f;
 
+    public  SkinnedMeshRenderer meshRenderer;
     private Rigidbody rigid;
     private BoxCollider bossCollider;
-    public  SkinnedMeshRenderer meshRenderer;
     private Animator animator;
 
     public int aniStateValue = 0;
@@ -42,58 +45,13 @@ public class Enemy : MonoBehaviour
         animator = GetComponent<Animator>();
         curHealth = maxHealth;
 
-        animator.SetInteger("AniState", (int)AniState.Sleep);
+        //animator.SetInteger("AniState", (int)AniState.Sleep);
+        StartCoroutine(Think());
     }
 
     private void Update()
     {
-        //test animation
-        if (isState == true)
-        {
-            
-            if (Input.GetKeyUp(KeyCode.F1))
-            {
-                StartCoroutine(SetAnimationState(AniState.Idle));
-            }
-            else if (Input.GetKeyUp(KeyCode.F2))
-            {
-                StartCoroutine(SetAnimationState(AniState.Walk));
-            }
-            else if (Input.GetKeyUp(KeyCode.F3))
-            {
-                StartCoroutine(SetAnimationState(AniState.Run));
-            }
-            else if (Input.GetKeyUp(KeyCode.F4))
-            {
-                StartCoroutine(SetAnimationState(AniState.IdleCombat));
-                
-            }
-            else if (Input.GetKeyUp(KeyCode.F5))
-            {
-                StartCoroutine(SetAnimationState(AniState.Death));
-            }
 
-
-            else if (Input.GetKeyUp(KeyCode.F6))
-            {
-                StartCoroutine(SetAnimationTrigger(AniState.GetHit));
-            }
-            else if (Input.GetKeyUp(KeyCode.F7))
-            {
-                StartCoroutine(SetAnimationTrigger(AniState.Attack1));
-
-            }
-            else if (Input.GetKeyUp(KeyCode.F8))
-            {
-                StartCoroutine(SetAnimationTrigger(AniState.Attack2));
-
-            }
-            else if (Input.GetKeyUp(KeyCode.F9))
-            {
-                StartCoroutine(SetAnimationTrigger(AniState.Buff));
-
-            }
-        }
     }
 
     // 현재 상태 걷기, 뛰기, 전투 모드 등등 지속적인 상태 구분
@@ -110,12 +68,12 @@ public class Enemy : MonoBehaviour
     // 공격 패턴 애니메이션 다시 이전 애니메이션으로 돌아가는 부분 예를 들어 공격하고 전투모드로
     IEnumerator SetAnimationTrigger(AniState state)
     {
-        isState = false;
+        isState = true;
         // AniState 열거형 멤버를 정수 값으로 변환하여 전달
         //Debug.Log(state.ToString());
         animator.SetTrigger("do" + state.ToString());
         yield return new WaitForSeconds(1f);
-        isState = true;
+        isState = false;
     }
 
 
@@ -125,26 +83,25 @@ public class Enemy : MonoBehaviour
         if (other.name == "SwordCollier" && !isDead)
         {
             StartCoroutine(OnDamage());
-            if(isState == true)
+            if(isState != true)
             {
                 StartCoroutine(SetAnimationTrigger(AniState.GetHit));
             }
         }
     }
 
-   IEnumerator OnDamage()
-   {
+    IEnumerator OnDamage()
+    {
         curHealth -= 10.0f;
-
-        StartCoroutine(SetAnimationState(AniState.GetHit));
         meshRenderer.material.color = Color.red;
+
         yield return new WaitForSeconds(0.1f);
 
-        if (  curHealth > 0 ) 
+        if (curHealth > 0)
         {
             meshRenderer.material.color = Color.white;
         }
-        else if ( curHealth <= 0 ) 
+        else if (curHealth <= 0)
         {
             // 죽었을때.
             StartCoroutine(Die());
@@ -160,4 +117,64 @@ public class Enemy : MonoBehaviour
         isDead = true;
         Destroy(this.gameObject);
     }
+
+    IEnumerator Think()
+    {
+        // 기본 Idle 상태에서 
+        StartCoroutine(SetAnimationState(AniState.IdleCombat));
+        yield return new WaitForSeconds(bossAniTime); // 기본 텀 시간
+
+        int ranAction = Random.Range(0, 4);
+        switch (ranAction)
+        {
+            case 0:
+            case 1:
+                StartCoroutine(Attack1());
+                break;
+            case 2:
+            case 3:
+                StartCoroutine (Attack2());
+                break;
+            case 4:
+                StartCoroutine(Buff());
+                break;
+        }
+    }
+
+    IEnumerator Attack1()
+    {
+        // RightAttack
+        isState = true;
+        StartCoroutine(SetAnimationTrigger(AniState.Attack1));
+        yield return new WaitForSeconds(bossAniTime);
+        isState = false;
+
+        StartCoroutine(Think());
+    }
+
+    IEnumerator Attack2()
+    {
+        // leftAttack
+        isState = false;
+        StartCoroutine(SetAnimationTrigger(AniState.Attack2));
+        yield return new WaitForSeconds(bossAniTime);
+        isState = true;
+
+        StartCoroutine(Think());
+    }
+
+    IEnumerator Buff()
+    {
+        // 피가 절반 때부터 발동이 가능한 상태
+        if(curHealth <= maxHealth/2)
+        {
+            isState = false;
+            StartCoroutine(SetAnimationTrigger(AniState.Buff));
+            yield return new WaitForSeconds(bossAniTime);
+            isState = true;
+        }
+
+        StartCoroutine(Think());
+    }
+
 }
